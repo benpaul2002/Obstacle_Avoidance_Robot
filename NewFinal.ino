630866c6d92f9ee7040d1c810cf977a977abf07d
Anirudh Kaushik <anirudhkaushik2003@gmail.com>
2022-09-24 13:25:49 +0530

    Update NewFinal.ino

--- NewFinal.ino
+++ NewFinal.ino
@@ -1,4 +1,6 @@
 #include <ArduinoHttpClient.h>
+#include <ArduinoMqttClient.h>
+
 //#include <WiFi101.h>
 #include <ArduinoJson.h>
 #include <TouchScreen.h>
@@ -7,30 +9,34 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 
+
 //#include "secrets.h"
 #include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
 
 
+
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 /////// WiFi Settings ///////
-char ssid[] = "Hotspot";
-char pass[] = "esw_lab10";
-
+//char ssid[] = "otspott";
+//char pass[] = "esw_team10";
+char ssid[] = "weeeee";
+char pass[] = "KiaraCoke";
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 //WiFiClient  client;
 
 unsigned long myChannelNumber = 1848245;
 const char * myWriteAPIKey = "WGU883V6XLSQR7ME";
-String cse_ip = "192.168.43.247"; // YOUR IP from ipconfig/ifconfig
+String cse_ip = "10.42.0.1"; // YOUR IP from ipconfig/ifconfig
 String cse_port = "8080";
-String serverName = "http://esw-onem2m.iiit.ac.in:443/~/in-cse/in-name/";
+String serverName = "http://esw-onem2m.iiit.ac.in:443";
 String ae = "ESW-ultrasonic-sensor";
 String cnt = "node1";
-char serverAddress[] = "192.168.43.247";  // server address
+char serverAddress[] = "10.42.0.1";  // server address
 int port = 443;
 
 WiFiServer server(80); 
 WiFiClient wifi = server.available();
+MqttClient mqttclient(wifi);
 
 int status = WL_IDLE_STATUS;
 
@@ -39,11 +45,12 @@ int trigPin = 4;
 int echoPin = 12;
 float duration;
 int i = 0;
+long onFlag = 0;
 
 #define SOUND_SPEED 0.034
 
 
-HttpClient client = HttpClient(wifi, "esw-onem2m.iiit.ac.in", port);
+HttpClient client1 = HttpClient(wifi, "esw-onem2m.iiit.ac.in", port);
 MotorDriver m;
 
 long duration1;                                                           //duration of ultrasonic pulse
@@ -54,11 +61,20 @@ int distanceCm2;
 int distanceCm3;
 String distance1, distance2, distance3, dir;
 
-void turnRight() {
-  m.motor(1, FORWARD, 0);
+void stop_car(){
+  m.motor(1, BACKWARD, 0);
   m.motor(2, FORWARD, 0); 
   m.motor(3, FORWARD, 0);
-  m.motor(4, FORWARD, 0);  
+  m.motor(4, BACKWARD, 0); 
+
+}
+
+void turnRight() {
+  m.motor(1, BACKWARD, 0);
+  m.motor(2, FORWARD, 0); 
+  m.motor(3, FORWARD, 0);
+  m.motor(4, BACKWARD, 0); 
+  
   m.motor(1, BACKWARD, 200);
   m.motor(2, FORWARD, 200); 
   m.motor(3, FORWARD, 200);
@@ -67,10 +83,10 @@ void turnRight() {
 
 void turnLeft() {
   m.motor(1, FORWARD, 0);
-  m.motor(2, FORWARD, 0); 
-  m.motor(3, FORWARD, 0);
+  m.motor(2, BACKWARD, 0); 
+  m.motor(3, BACKWARD, 0);
   m.motor(4, FORWARD, 0);  
-
+  
   m.motor(1, FORWARD, 200);
   m.motor(2, BACKWARD, 200); 
   m.motor(3, BACKWARD, 200);
@@ -85,37 +101,80 @@ void goForward() {
 }
 
 void goBackward() {
+
   m.motor(1, BACKWARD, 200);
   m.motor(2, BACKWARD, 200);
   m.motor(3, BACKWARD, 200);
   m.motor(4, BACKWARD, 200);  
 }
 
+void thingspeak_write(String dir){
+  ThingSpeak.setField(1, distance1);
+  ThingSpeak.setField(2, distance2);
+  ThingSpeak.setField(3, distance3);
+  ThingSpeak.setField(4, dir);
+}
 
+void ping_sensor(){
+  digitalWrite(A1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(A1, HIGH);                                                 //give a pulse of 10us on TRIG
+  delayMicroseconds(10);
+  digitalWrite(A1, LOW);
+  duration1 = pulseIn(A0, HIGH);                                          //check time elasped in receiving back the pulse on ECHO
+  delay(50);
+  digitalWrite(A3, LOW);
+  delayMicroseconds(2);
+  digitalWrite(A3, HIGH);                                                 //give a pulse of 10us on TRIG
+  delayMicroseconds(10);
+  digitalWrite(A3, LOW);
+  duration2 = pulseIn(A2, HIGH);
+  delay(50);
+  digitalWrite(A5, LOW);
+  delayMicroseconds(2);
+  digitalWrite(A5, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(A5, LOW);
+  duration3 = pulseIn(A4, HIGH);
+  delay(50);
 
+  distanceCm1 = (duration1 * 0.034 )/ 2;                                    //convert to distance in cm
+  distanceCm2 = (duration2 * 0.034 )/ 2;
+  distanceCm3 = (duration3 * 0.034 )/ 2;
 
-
-
+  Serial.println(distanceCm1);
+  Serial.println(distanceCm2);
+  Serial.println(distanceCm3);
+}
 void createCI(String& val){
 // add the lines in step 3-6 inside this function
 
 
-  String postData = "{\"m2m:cin\": {\"lbl\": [ \"Team-16\" ],\"con\": \"" + String(val)+ "\"}}";
-
+  String postData2 = "{\"m2m:cin\": {\"lbl\": [ \"Team-16\" ],\"con\": \"45\"}}";
+  Serial.println(postData2);
+  String postData = "{\"m2m:cin\": {\"lbl\": [ \"Team-16\" ],\"con\": ";
+  postData += "\"";
+  postData += String(val);
+  postData += "\"";
+  postData += "}}";
   Serial.println(postData);
-  client.beginRequest();
-  client.post("/~/in-cse/in-name/Team-16/Node-1/Data");
-  client.sendHeader("Content-Type", "application/json;ty=4");
-  client.sendHeader("X-M2M-Origin", "w961iw:JEhiH5");
-//  client.sendHeader("Content-Length", String(postData.length()));
-  client.sendHeader("Connection", "keep-alive");
-  client.beginBody();
-  client.print(postData);
-  client.endRequest();
+//  auto var = std::format("{\"m2m:cin\": {\"lbl\": [ \"Team-16\" ],\"con\": \"{}\"}}",val);
+//  Serial.println(var);
+
+  
+  client1.beginRequest();
+  client1.post("/~/in-cse/in-name/Team-16/Node-1/Data");
+  client1.sendHeader("Content-Type", "application/json;ty=4");
+  client1.sendHeader("X-M2M-Origin", "w961iw:JEhiH5");
+  client1.sendHeader("Content-Length", String(postData.length()));
+//  client.sendHeader("Connection", "keep-alive");
+  client1.beginBody();
+  client1.print(postData);
+  client1.endRequest();
 
 //  // read the status code and body of the response
-  int statusCode = client.responseStatusCode();
-  String response = client.responseBody();
+  int statusCode = client1.responseStatusCode();
+  String response = client1.responseBody();
 
   Serial.print("Status code: ");
   Serial.println(statusCode);
@@ -172,7 +231,7 @@ void connect_WiFi() {
 
 // time code
 long previousMillis = 0;
-long interval = 5000;
+long interval = 30000;
 
 void setup() {
   pinMode(A1, OUTPUT);                                                    //Analog pin A1 connected to TRIG
@@ -203,91 +262,110 @@ void setup() {
   
 }
 
+void navigate(){
+  if(distanceCm2 > distanceCm3 && distanceCm2 <= 1000) {
+      turnLeft();
+      distance1 = String(distanceCm1);
+      distance2 = String(distanceCm2);
+      distance3 = String(distanceCm3);
+      dir = String(3);
+      thingspeak_write(dir);
+      delay(1500);
+      
+    }
+    if(distanceCm3 > distanceCm2 && distanceCm3 <= 1000) {
+      turnRight();
+      distance1 = String(distanceCm1);
+      distance2 = String(distanceCm2);
+      distance3 = String(distanceCm3);
+      dir = String(2);
+      thingspeak_write(dir);
+      delay(1500);
+    }
+    ping_sensor();
+    if(distanceCm3 >= 1000 ||distanceCm2 >= 1000  )                                                   //if distance less than 25cm display on monitor
+    {
+  
+        stop_car();
+        delay(1000);
+        goBackward();
+        delay(2500);
+        stop_car();
+        delay(1000);
+        navigate();
+    }
+}
+
 void loop()
 {
-  digitalWrite(A1, LOW);
-  delayMicroseconds(2);
-  digitalWrite(A1, HIGH);                                                 //give a pulse of 10us on TRIG
-  delayMicroseconds(10);
-  digitalWrite(A1, LOW);
-  duration1 = pulseIn(A0, HIGH);                                          //check time elasped in receiving back the pulse on ECHO
-  delay(100);
-  digitalWrite(A3, LOW);
-  delayMicroseconds(2);
-  digitalWrite(A3, HIGH);                                                 //give a pulse of 10us on TRIG
-  delayMicroseconds(10);
-  digitalWrite(A3, LOW);
-  duration2 = pulseIn(A2, HIGH);
-  delay(100);
-  digitalWrite(A5, LOW);
-  delayMicroseconds(2);
-  digitalWrite(A5, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(A5, LOW);
-  duration3 = pulseIn(A4, HIGH);
-  delay(100);
+  Serial.println(onFlag +"running");onFlag = ThingSpeak.readLongField(1856320, 1, "NUAMJ7MY4VAX7V8U");
+  while(onFlag == 0) {
+    onFlag = ThingSpeak.readLongField(1856320, 1, "NUAMJ7MY4VAX7V8U");
+    Serial.println("0 Waiting");
+    m.motor(1, FORWARD, 0);
+    m.motor(2, FORWARD, 0); 
+    m.motor(3, FORWARD, 0);
+    m.motor(4, FORWARD, 0);
+
+  }
+  ping_sensor();
   m.motor(1, FORWARD, 200);
   m.motor(2, FORWARD, 200); 
   m.motor(3, FORWARD, 200);
   m.motor(4, FORWARD, 200);
-  distanceCm1 = (duration1 * 0.034 )/ 2;                                    //convert to distance in cm
-  distanceCm2 = (duration2 * 0.034 )/ 2;
-  distanceCm3 = (duration3 * 0.034 )/ 2;
+
+
+  
 
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis > interval){
     Serial.println("30s over");
     previousMillis = currentMillis;
-    String val = String(distanceCm1);
+    String val = "[";
+    val += String(distanceCm1);
+    val += ",";
+    val += String(distanceCm2);
+    val += ",";
+    val += String(distanceCm3);
+    val += "]";
+    val += ",";
+    val += String(dir);
+
+    Serial.println(val);
+    
     createCI(val);
   }
   
-  if(distanceCm1 <= 10)                                                   //if distance less than 20cm display on monitor
+  if(distanceCm2 <= 75 || distanceCm3 <= 75 )                                                   //if distance less than 25cm display on monitor
   {
-      if(distanceCm2 <= 10 && distanceCm3 <= 10) {
-          goBackward();
-          distance1 = String(distanceCm1);
-          distance2 = String(distanceCm2);
-          distance3 = String(distanceCm3);
-          dir = String(4);
-          ThingSpeak.setField(1, distance1);
-          ThingSpeak.setField(2, distance2);
-          ThingSpeak.setField(3, distance3);
-          ThingSpeak.setField(4, "4");
-      }
-      else if(distanceCm3 <= 10) {
-          turnLeft();
-          distance1 = String(distanceCm1);
-          distance2 = String(distanceCm2);
-          distance3 = String(distanceCm3);
-          dir = String(3);
-          ThingSpeak.setField(1, distance1);
-          ThingSpeak.setField(2, distance2);
-          ThingSpeak.setField(3, distance3);
-          ThingSpeak.setField(4, "3");
-      }
-      else if(distanceCm2 <= 10) {
-        turnRight();
-        distance1 = String(distanceCm1);
-        distance2 = String(distanceCm2);
-        distance3 = String(distanceCm3);
-        dir = String(2);
-        ThingSpeak.setField(1, distance1);
-        ThingSpeak.setField(2, distance2);
-        ThingSpeak.setField(3, distance3);
-        ThingSpeak.setField(4, "2");
-      }
+
+      stop_car();
+      delay(1000);
+      goBackward();
+      delay(2500);
+      stop_car();
+      delay(1000);
+      
+      navigate();
+
+      goForward();
+      distance1 = String(distanceCm1);
+      distance2 = String(distanceCm2);
+      distance3 = String(distanceCm3);
+      dir = String(1);
+      thingspeak_write(dir);
+
+      
   }
+//      
   else {
       goForward();
       distance1 = String(distanceCm1);
       distance2 = String(distanceCm2);
       distance3 = String(distanceCm3);
       dir = String(1);
-      ThingSpeak.setField(1, distance1);
-      ThingSpeak.setField(2, distance2);
-      ThingSpeak.setField(3, distance3);
-      ThingSpeak.setField(4, "1");
+      thingspeak_write(dir);
+
   }
   int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
 }
